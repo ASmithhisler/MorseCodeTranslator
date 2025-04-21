3b5ac3b3eadb8c53f9322623a7c512cca7dc06b4
Alex Smithhisler <asmithhisler@hotmail.com>
2025-04-21 17:40:23 -0400

    Added the encoding function for the main file.

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas
+
+data = pandas.read_csv("morse_code.csv")
+morse_code_dict = {row.char: row.code for (index, row) in data.iterrows()}
+
+string = input("").upper()
+translation_list = []
+
+for word in string.split():
+    for char in word:
+        try:
+            translated_char = morse_code_dict[char]
+        except KeyError:
+            translated_char = morse_code_dict["Err"]
+        translation_list.append(translated_char)
+    translation_list.append(morse_code_dict[" "])
+
+translation = ""
+for char in translation_list:
+    translation += char + " "
+
+print(translation)
