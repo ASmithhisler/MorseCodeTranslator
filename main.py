cce7bcb6e46bd4a9927e2661980c5a8db461bcbd
Alex Smithhisler <asmithhisler@hotmail.com>
2025-04-21 18:57:19 -0400

    Added a repeat function to the main file.

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import pandas
 data = pandas.read_csv("morse_code.csv")
 morse_code_dict = {row.char: row.code for (index, row) in data.iterrows()}
 
-string = input("Type your message: ").upper()
-translation_list = []
+retry = True
+while retry == True:
+    string = input("Type your message: ").upper()
+    translation_list = []
 
-for word in string.split():
-    for char in word:
-        try:
-            translated_char = morse_code_dict[char]
-        except KeyError:
-            translated_char = morse_code_dict["Err"]
-        translation_list.append(translated_char)
-    translation_list.append(morse_code_dict[" "])
+    for word in string.split():
+        for char in word:
+            try:
+                translated_char = morse_code_dict[char]
+            except KeyError:
+                translated_char = morse_code_dict["Err"]
+            translation_list.append(translated_char)
+        translation_list.append(morse_code_dict[" "])
 
-translation = ""
-for char in translation_list:
-    translation += char + " "
+    translation = ""
+    for char in translation_list:
+        translation += char + " "
 
-print(f"The translation is: {translation}")
+    print(f"The translation is: {translation}")
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    print()
+    if restart == "no":
+        retry = False
