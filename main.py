1a5d51aeed95bbf0bc9db25f899f8457fd33d739
Alex Smithhisler <asmithhisler@hotmail.com>
2025-04-21 20:25:10 -0400

    Added the decode function to the main file.

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 import pandas
 from art import logo
 
-data = pandas.read_csv("morse_code.csv")
-morse_code_dict = {row.char: row.code for (index, row) in data.iterrows()}
 
-print(logo)
-
-retry = True
-while retry == True:
-    string = input("Type your message: ").upper()
-    translation_list = []
-
+def encode(string, translation_list):
+    first = True
     for word in string.split():
+        if not first:
+            translation_list.append(morse_code_dict[" "])
+        first = False
         for char in word:
             try:
                 translated_char = morse_code_dict[char]
             except KeyError:
                 translated_char = morse_code_dict["Err"]
             translation_list.append(translated_char)
-        translation_list.append(morse_code_dict[" "])
+    return translation_list
+
+
+def decode(string, translation_list):
+    for code in string.split():
+        try:
+            translated_char = text_dict[code]
+        except KeyError:
+            translated_char = text_dict["#"]
+        translation_list.append(translated_char)
+    return translation_list
+
+
+data = pandas.read_csv("morse_code.csv")
+morse_code_dict = {row.char: row.code for (index, row) in data.iterrows()}
+text_dict = {row.code: row.char for (index, row) in data.iterrows()}
+
+print(logo)
+
+retry = True
+while retry == True:
+    func = input("Type 'e' to encode, type 'd' to decode: ")
+    string = input("Type your message: ").upper()
+
+    translation_list = []
+    if func == "e":
+        translation_list = encode(string, translation_list)
+    if func == "d":
+        translation_list = decode(string, translation_list)
 
     translation = ""
     for char in translation_list:
@@ -26,7 +50,9 @@ while retry == True:
 
     print(f"The translation is: {translation}")
 
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    restart = input("Type 'y' if you want to go again: ").lower()
     print()
-    if restart == "no":
+    if restart == "y":
+        retry = True
+    else:
         retry = False
