6f5224acaee1d1187e1f67a46be88da7cc401f0d
Alex Smithhisler <asmithhisler@hotmail.com>
2025-04-29 17:51:23 -0400

    Added the data.py file and fixed some issues.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas
 from art import logo
+from data import morse_code_dict
 
 
 def encode(string, translation_list):
@@ -12,7 +12,7 @@ def encode(string, translation_list):
             try:
                 translated_char = morse_code_dict[char]
             except KeyError:
-                translated_char = morse_code_dict["Err"]
+                translated_char = morse_code_dict[""]
             translation_list.append(translated_char)
     return translation_list
 
@@ -27,60 +27,6 @@ def decode(string, translation_list):
     return translation_list
 
 
-morse_code_dict = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "0": "-----",
-    "!": "-.-.--",
-    "@": ".--.-.",
-    "(": "-.--.",
-    ")": "-.--.-",
-    "-": "-....-",
-    "_": "..--.-.",
-    "=": ".-.-.",
-    "+": "-...-",
-    ";": "-.-.-.",
-    ":": "---...",
-    ",": "--..--",
-    ".": ".-.-.-",
-    "/": "-..-.",
-    "?": "..--..",
-    " ": "/",
-    "Err": "#",
-}
 text_dict = {v: k for k, v in morse_code_dict.items()}
 
 print(logo)
@@ -91,23 +37,23 @@ while retry == True:
     string = input("Type your message: ").upper()
 
     translation_list = []
-    if func == "e":
-        translation_list = encode(string, translation_list)
     if func == "d":
         translation_list = decode(string, translation_list)
+    else:
+        translation_list = encode(string, translation_list)
 
     translation = ""
     for char in translation_list:
-        if func == "e":
-            translation += char + " "
         if func == "d":
             translation += char
+        else:
+            translation += char + " "
 
     print(f"The translation is: {translation}")
 
     restart = input("Type 'y' if you want to go again: ").lower()
-    print()
     if restart == "y":
+        print()
         retry = True
     else:
         retry = False
